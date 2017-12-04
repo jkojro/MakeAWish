7286dc0485dc358b4128fb1b71e107e2fc96a1fe
jkojro <janusz.kojro@gmail.com>
2017-12-04 22:07:15 +0100

    Pytania w wish/show

--- app/controllers/querries_controller.rb
+++ app/controllers/querries_controller.rb
@@ -1,2 +1,16 @@
 class QuerriesController < ApplicationController
+    
+    def create
+        @querry = Querry.new(querry_params)
+        @querry.wish_id = params[:wish_id]
+        
+        @querry.save
+        
+        redirect_to wish_path(@querry.wish)
+    end
+    
+    def querry_params
+        params.require(:querry).permit(:question, :answer, :tip)
+    end
+    
 end
